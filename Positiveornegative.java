af198f9be27c841f129abf7473ee836a541592bc
AswiniAnjappan <aswini.acse@ksrce.ac.in>
2016-07-20 16:54:41 +0530

    Create Positiveornegative.java

---
+++ Positiveornegative.java
@@ -0,0 +1,27 @@
+package wipro;
+
+import java.util.Scanner;
+
+public class Positiveornegative {
+
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number: ");
+		int x=s.nextInt();
+		if(x<0){
+			System.out.println("negative");
+		}
+		else if(x>0){
+			System.out.println("positive");
+		}
+		else if(x==0){
+			System.out.println("Zero");
+		}
+		else{
+			System.out.println("invalid number");
+		}
+
+	}
+
+}
