c566dc57f496d68781aa4e97755a5fbdb5260fb1
AswiniAnjappan <aswini.acse@ksrce.ac.in>
2016-07-23 10:37:51 +0530

    Create NoOftwos.java

---
+++ NoOftwos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class NoOftwos {
+
+	
+	public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the number: ");
+	int x=s.nextInt();
+	int count=0;
+	while(x>=2){
+		x=x-2;
+		count++;
+	}
+System.out.println("The no of two's are: "+count);
+	}
+
+}
